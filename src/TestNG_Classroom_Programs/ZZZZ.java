515cb72534e81ffed624e266de4c2cb2e2043de0
Charvik <Charvik@LAPTOP-S0IS0QVR>
2021-07-23 19:17:12 +0530

    Save

---
+++ src/TestNG_Classroom_Programs/ZZZZ.java
@@ -0,0 +1,19 @@
+package TestNG_Classroom_Programs;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class ZZZZ {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
