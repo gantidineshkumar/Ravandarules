418fa098b3547ffc59fa106f38c6bdd7fff105bc
Charvik <Charvik@LAPTOP-S0IS0QVR>
2021-07-23 19:31:26 +0530

    3333 push

---
+++ src/TestNG_Classroom_Programs/Bhanu.java
@@ -0,0 +1,19 @@
+package TestNG_Classroom_Programs;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Bhanu {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
---
+++ src/TestNG_Classroom_Programs/Camel.java
@@ -0,0 +1,19 @@
+package TestNG_Classroom_Programs;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Camel {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
